2bf578615a1de24382f867a8761c80c05af82950
perring <perrin.grandne@gmail.com>
2024-01-15 22:37:35 +0100

    update game

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,41 +1,66 @@
-import { Gltf, Stats, OrbitControls, Circle, KeyboardControls } from '@react-three/drei'
+import {
+	Gltf,
+	Stats,
+	OrbitControls,
+	Circle,
+	KeyboardControls,
+} from '@react-three/drei'
 import { Canvas, useLoader } from '@react-three/fiber'
 import Ecctrl from 'ecctrl'
 import { Physics, RigidBody } from '@react-three/rapier'
-import { Suspense } from 'react';
+import { Suspense } from 'react'
 
 export function Game() {
-  const keyboardMap = [
-    { name: 'forward',  keys: ['ArrowDown', 'KeyS']},
-    { name: 'backward', keys: ['ArrowUp', 'KeyW']  },
-    { name: 'leftward', keys: ['ArrowRight', 'KeyD']},
-    { name: 'rightward', keys: ['ArrowLeft', 'KeyA']  },
-    { name: 'jump', keys: ['Space'] },
-    { name: 'run', keys: ['Shift'] },
-  ]
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Canvas shadows onPointerDown={(e) => e.target.requestPointerLock()}>
-       <directionalLight intensity={0.7} castShadow shadow-bias={-0.0004} position={[-20, 20, 20]}>
-      <orthographicCamera attach="shadow-camera" args={[-20, 20, 20, -20]} />
-      </directionalLight>
-      <ambientLight intensity={0.2} />
-  <Physics timeStep="vary">
-    <KeyboardControls map={keyboardMap}>
-      {/* <Controller maxVelLimit={5}> */}
-      <Ecctrl camInitDis= {4} >
-        <Gltf castShadow receiveShadow scale={0.315} position={[0, -0.55, 0]} src="/models/ghost_w_tophat-transformed.glb" />
-        </Ecctrl>
-      {/* </Controller> */}
-    </KeyboardControls>
-    <RigidBody type="fixed" colliders="trimesh">
-        <Gltf castShadow receiveShadow position={[0,-2,2]} scale={.8} src="/models/japanese_street_at_night_s.glb" />
-     </RigidBody>
-    </Physics> 
-</Canvas>
-</Suspense>
-  )
-
-
-
+	const keyboardMap = [
+		{ name: 'forward', keys: ['ArrowDown', 'KeyS'] },
+		{ name: 'backward', keys: ['ArrowUp', 'KeyW'] },
+		{ name: 'leftward', keys: ['ArrowRight', 'KeyD'] },
+		{ name: 'rightward', keys: ['ArrowLeft', 'KeyA'] },
+		{ name: 'jump', keys: ['Space'] },
+		{ name: 'run', keys: ['Shift'] },
+	]
+	return (
+		<Suspense fallback={<div>Loading...</div>}>
+			<div>
+				<Canvas shadows>
+					<directionalLight
+						intensity={0.7}
+						castShadow
+						shadow-bias={-0.0004}
+						position={[-20, 20, 20]}
+					>
+						<orthographicCamera
+							attach='shadow-camera'
+							args={[-20, 20, 20, -20]}
+						/>
+					</directionalLight>
+					<ambientLight intensity={0.2} />
+					<Physics timeStep='vary'>
+						<KeyboardControls map={keyboardMap}>
+							{/* <Controller maxVelLimit={5}> */}
+							<Ecctrl camInitDis={4}>
+								<Gltf
+									castShadow
+									receiveShadow
+									scale={0.315}
+									position={[0, -0.55, 0]}
+									src='/models/ghost_w_tophat-transformed.glb'
+								/>
+							</Ecctrl>
+							{/* </Controller> */}
+						</KeyboardControls>
+						<RigidBody type='fixed' colliders='trimesh'>
+							<Gltf
+								castShadow
+								receiveShadow
+								position={[0, -2, 2]}
+								scale={0.8}
+								src='/models/japanese_street_at_night_s.glb'
+							/>
+						</RigidBody>
+					</Physics>
+				</Canvas>
+			</div>
+		</Suspense>
+	)
 }
