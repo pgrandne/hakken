367a4014a97279c47bd6244273bbdbddaa640231
perring <perrin.grandne@gmail.com>
2024-01-20 22:06:00 +0100

    deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,48 @@
 'use client'
 
-// import { ConnectKitButton } from '../components/ConnectKitButton'
-// import { Game } from '../components/Game/Game'
-// import { useAccount, useNetwork } from 'wagmi'
-// import { useState } from 'react'
-// import { ModalBridge, ModalFaucet, ModalReward } from '../components/Modal'
-// import Image from 'next/image'
-// import { Raleway } from 'next/font/google'
-// import { goerli, sepolia } from 'wagmi/chains'
-// import { ToastContainer } from 'react-toastify'
+import { ConnectKitButton } from '../components/ConnectKitButton'
+import { Game } from '../components/Game/Game'
+import { useAccount, useNetwork } from 'wagmi'
+import { useState } from 'react'
+import { ModalBridge, ModalFaucet, ModalReward } from '../components/Modal'
+import Image from 'next/image'
+import { Raleway } from 'next/font/google'
+import { goerli, sepolia } from 'wagmi/chains'
 
-// const raleway = Raleway({
-// 	weight: '400',
-// 	subsets: ['latin'],
-// })
+const raleway = Raleway({
+	weight: '400',
+	subsets: ['latin'],
+})
 
 export default function Home() {
-	// const { address, isConnected } = useAccount()
-	// const { chain } = useNetwork()
-	// const [modal, setModal] = useState({
-	// 	bridge: false,
-	// 	faucet: false,
-	// 	reward: false,
-	// })
+	const { address, isConnected } = useAccount()
+	const { chain } = useNetwork()
+	const [modal, setModal] = useState({
+		bridge: false,
+		faucet: false,
+		reward: false,
+	})
 
 	return (
 		<>
-			test
-			{/* <div className={`absolute ${!isConnected ? 'top-4 animate-bounce' : ''}`}>
+			<div className={`absolute ${!isConnected ? 'top-4 animate-bounce' : ''}`}>
 				<Image
 					src='/images/ghosty.png'
 					width={70}
 					height={70}
 					alt='Picture of the author'
 				/>
-			</div> */}
-			{/* <h1
+			</div>
+			<h1
 				className={`absolute w-screen top-3 text-5xl text-center text-transparent bg-clip-text bg-gradient-to-r from-black to-violet-300 ${raleway.className}`}
 			>
 				HAKKEN 発見
-			</h1> */}
-			{/* <div className='absolute top-3 right-3'>
+			</h1>
+			<div className='absolute top-3 right-3'>
 				<ConnectKitButton theme='nouns' />
-			</div> */}
-			{/* <div className='h-screen w-screen flex justify-center items-center'> */}
-			{/* {isConnected &&
+			</div>
+			<div className='h-screen w-screen flex justify-center items-center'>
+				{isConnected &&
 				address &&
 				chain &&
 				(chain.id === sepolia.id || chain.id === goerli.id) ? (
@@ -53,17 +51,17 @@ export default function Home() {
 					<div className={`text-lg text-white ${raleway.className}`}>
 						Please Connect to play
 					</div>
-				)} */}
-			{/* </div> */}
-			{/* {address && modal.bridge && (
+				)}
+			</div>
+			{address && modal.bridge && (
 				<ModalBridge address={address} setModal={setModal} />
 			)}
-			{address && !modal.faucet && (
+			{address && modal.faucet && (
 				<ModalFaucet address={address} setModal={setModal} />
 			)}
 			{address && modal.reward && (
 				<ModalReward address={address} setModal={setModal} />
-			)} */}
+			)}
 		</>
 	)
 }
