6e466f5976176f80c3a7bd4969f48d06f1d3e534
perring <perrin.grandne@gmail.com>
2024-01-22 17:46:19 +0100

    Change name balance from Arbitrum Sepolia to Arb Sepo

--- src/components/Modal/components/BalanceArbitrum.tsx
+++ src/components/Modal/components/BalanceArbitrum.tsx
@@ -18,7 +18,7 @@ export const BalanceArbitrum = ({ address }: { address: `0x${string}` }) => {
 	return (
 		<div className='flex flex-col w-4/12'>
 			<div className='bg-[#f8f8f836] rounded-md p-2 text-center border border-gray-400 box-shadow-custom'>
-				<p className='text-gray-600 font-bold mb-1'>Arbitrum Sepolia</p>
+				<p className='text-gray-600 font-bold mb-1'>Arb Sepo</p>
 				<span className='balance py-1 px-4 rounded-md border w-full font-light text-sm'>
 					{balanceWithoutDecimals} {data?.symbol}
 				</span>
